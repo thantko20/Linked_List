300c73d41d66f2a45073b1edba420c984b35d599
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-12 01:06:09 +0630

    Refactor #append by using #tail

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,9 +8,10 @@ class LinkedList
   end
  
   def append(value)
-    tmp = @head
-    tmp = tmp.next_node while !tmp.next_node.nil?
-    tmp.next_node = Node.new(value)
+    #tmp = @head
+    #tmp = tmp.next_node while !tmp.next_node.nil?
+    #tmp.next_node = Node.new(value)
+    tail.next_node = Node.new(value)
   end
 
   def prepend(value)
