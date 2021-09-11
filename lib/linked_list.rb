cd7b5d591dde83c1224e95449e1fcabe396d061d
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-12 00:28:26 +0630

    Add #prepend(value) and remove pseudocode for this

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 require_relative 'node'
+require 'pry-byebug'
 
 class LinkedList
   attr_accessor :head
@@ -6,20 +7,15 @@ class LinkedList
     @head = Node.new(value)
   end
   
-  # Implement #append(value) which adds a new node with value
-  # to the end of the list
-  # End of the list can be found at the node which #next_node is nil
   def append(value)
-    # How to iterate through the list to find the last node?
-    # Create a tmp node and set it to the head node initially
-    # if the next node of current node isnt nil,
-    # set tmp to tmp.next
-    # if it is nil, set it to the node provided
-    tmp = head
+    tmp = @head
     tmp = tmp.next_node while !tmp.next_node.nil?
     tmp.next_node = Node.new(value)
   end
 
-  # Implement #prepend(value) that'll add a new node
-  # at the start of the list
+  def prepend(value)
+    tmp = @head
+    @head = Node.new(value)
+    @head.next_node = tmp
+  end
 end
