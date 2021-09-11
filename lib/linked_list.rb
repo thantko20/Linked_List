24ed7075f4a603dc087badfb9b7a33598e81be4c
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-11 23:22:36 +0630

    Add #append(value) which adds new node at the last of the list

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,8 +1,22 @@
 require_relative 'node'
 
 class LinkedList
-  attr_reader :head
+  attr_accessor :head
   def initialize(head=Node.new)
     @head = head
+  end
+  
+  # Implement #append(value) which adds a new node with value
+  # to the end of the list
+  # End of the list can be found at the node which #next_node is nil
+  def append(node)
+    # How to iterate through the list to find the last node?
+    # Create a tmp node and set it to the head node initially
+    # if the next node of current node isnt nil,
+    # set tmp to tmp.next
+    # if it is nil, set it to the node provided
+    tmp = head
+    tmp = tmp.next_node while !tmp.next_node.nil?
+    tmp.next_node = node
   end
 end
