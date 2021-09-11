0edb013a8aae7a74941c5e0d1680c37b183e958d
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-11 23:15:37 +0630

    attr_reader to attr_accessor

--- lib/node.rb
+++ lib/node.rb
@@ -2,7 +2,7 @@
 # both will be set to nil by default
 
 class Node
-  attr_reader :value, :next_node
+  attr_accessor :value, :next_node
   def initialize(value=nil, next_node=nil)
     @value = value
     @next_node = next_node
