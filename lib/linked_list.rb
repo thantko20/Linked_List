cb496382be2b221a2018458905062ff940065b51
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-11 23:24:28 +0630

    #append now takes value as parameter not node

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,7 +9,7 @@ class LinkedList
   # Implement #append(value) which adds a new node with value
   # to the end of the list
   # End of the list can be found at the node which #next_node is nil
-  def append(node)
+  def append(value)
     # How to iterate through the list to find the last node?
     # Create a tmp node and set it to the head node initially
     # if the next node of current node isnt nil,
@@ -17,6 +17,6 @@ class LinkedList
     # if it is nil, set it to the node provided
     tmp = head
     tmp = tmp.next_node while !tmp.next_node.nil?
-    tmp.next_node = node
+    tmp.next_node = Node.new(value)
   end
 end
