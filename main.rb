53b6e98ee86360e54fa5b283e33de5fddbac8a4b
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-12 12:15:03 +0630

    Test cases

---
+++ main.rb
@@ -0,0 +1,25 @@
+require_relative './lib/linked_list'
+
+list = LinkedList.new('Marco')
+
+list.append('Aaron')
+list.append('Susan')
+list.prepend("Amy")
+list.prepend("Josh")
+
+p list.sizem # => 5
+p list.tail.value # => "Susan"
+p list.at(2).value # => "Marco"
+
+p list.to_s # => "( Josh ) -> ( Amy ) -> ( Marco ) -> ( Aaron ) -> ( Susan ) ->  nil"
+
+list.insert_at('Mike', 1)
+
+p list.to_s # => "( Josh ) -> ( Mike ) -> ( Amy ) -> ( Marco ) -> ( Aaron ) -> ( Susan ) ->  nil"
+
+list.remove_at(1)
+
+p list.to_s # => "( Josh ) -> ( Amy ) -> ( Marco ) -> ( Aaron ) -> ( Susan ) ->  nil"
+p list.find('Susan') # => 4
+p list.contains?('fdas') # => false
+p list.contains?('Amy') # => true
